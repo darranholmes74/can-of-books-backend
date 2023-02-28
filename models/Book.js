0381254150f0d8d0059ecf6b71ec243ebef4e561
Ethan Storm <stormethan10@gmail.com>
2023-02-28 14:01:12 -0800

    added lib folder and began new routes

--- models/Book.js
+++ models/Book.js
@@ -11,35 +11,4 @@ const bookSchema = new mongoose.Schema({
 
 Book = mongoose.model('Book', bookSchema)
 
-async function seed(){
-  await Book.create({
-        title: 'Harry Potter',
-        author: 'J. K. Rowling',
-        description: 'Orphan Harry learns he is a wizard on his 11th birthday when Hagrid escorts him to magic-teaching Hogwarts School. As a baby, his mothers love protected him and vanquished the villain Voldemort, leaving the child famous as The Boy who Lived. With his friends Hermione and Ron, Harry has to defeat the returned He Who Must Not Be Named.',
-        status: {Sheldon: 'Sheldon: Watched the movies', Darran: 'Listened to the audio books', Ethan: 'Watched the movie'}
-        });
-    
-       console.log('saved Harry Potter')
-
-    await Book.create({
-        title: 'Lord of the Rings',
-        author: 'J. R. R. Tolkien',
-        description: 'The Lord of the Rings is an epic high-fantasy novel by English author and scholar J. R. R. Tolkien.',
-        status: {Sheldon: 'Sheldon: Watched the movies', Darran: 'Watched the movies', Ethan: 'Watched the movie'}
-    });
-    console.log('saved Lord of the Rings');
-
-    await Book.create({
-        title: 'Fablehaven',
-        author: 'Brandon Mull',
-        description: 'For centuries mystical creatures of all description were gathered into a hidden refuge called Fablehaven to prevent their extinction. The sanctuary survives today as one of the last strongholds of true magic.',
-        status: {Sheldon: 'never heard of it', Darran: 'never heard of it', Ethan: 'Read all the books'}
-    });
-    console.log('save Fablehaven')
-
-    mongoose.disconnect();
-}
-
-// seed();
-
 module.exports = Book
--- server.js
+++ server.js
@@ -1,26 +1,62 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
-app.use(cors());
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+app.use(cors())
+app.use(express.json())
 const Book = require('./models/Book')
 
-mongoose.connect(process.env.DATABASE_URL);
+const seed = require('./lib/seed')
 
+mongoose.connect(process.env.DATABASE_URL)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-app.get('/books', async (request, response) => {
-  const books = await Book.find()
-  response.send(books);
-  // await Book.deleteMany({title: 'Harry Potter'})
-  // await Book.deleteMany({title: 'Lord of the Rings'});
-  // await Book.deleteMany({title: 'Fablehaven'});
-});
+app.get('/', (req, res) => {
+	res.send('404 - page not found try /books')
+})
 
+// Seed the database with default books when this route is accessed
+app.get('/books/seed', async (request, response) => {
+	// return a list of books in the database
+	const books = await Book.find() // returns [] if no books
 
+	// if there are no books in the array returned seed the database
+	if (books.length < 1) {
+		seed() // seed the database with base data
+		response.status(201).send('Successfully seeded books into database') // 201 means created successfully
+	} else {
+		response.status(200).send('Books exist, seeding skipped') // if books exist send a message
+	}
+})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// get all books
+app.get('/books/all', async (request, response) => {
+	const books = await Book.find()
+	response.status(200).send(books)
+})
+
+// delete one specific book
+app.delete('/books/delete/:id', async (request, response) => {
+	// request.params Comes from the query string
+	// like -> /books/:id = req.params.id
+
+	// mongoose models have the findOne() method to search by a value like title:
+	const bookToDelete = await Book.findOne({ title: request.params.id })
+
+	try {
+		// wait for mongoose to delete the specified book from the Book collection
+		await Book.deleteOne(bookToDelete)
+		response.status(200).send('deleted')
+	} catch (error) {
+		console.log(`Error deleting ${error}`)
+		response
+			.status(200)
+			.send(`could not find a book called ${request.params.id}`)
+	}
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
