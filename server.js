f578ec0b9b2e40cd267843cb9bcb96248f06dcf2
Sheldon Pierce <Pierce55@icloud.com>
2023-02-27 17:30:13 -0800

    Minor updates

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ mongoose.connect(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', async (request, response) => {
+app.get('/books', async (request, response) => {
   const books = await Book.find()
-  response.send(`'test request received', ${books}`);
+  response.send(books);
   // await Book.deleteMany({title: 'Harry Potter'})
   // await Book.deleteMany({title: 'Lord of the Rings'});
   // await Book.deleteMany({title: 'Fablehaven'});
